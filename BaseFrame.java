e5c95bb02479068142ab845914ad01b9b26770c9
Eya Jamue Haque <147173274+eyajamuehaque@users.noreply.github.com>
2024-05-12 01:26:29 +0600

    Add files via upload

---
+++ BaseFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class BaseFrame extends JFrame {
+    public BaseFrame(String title) {
+        super(title);
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
