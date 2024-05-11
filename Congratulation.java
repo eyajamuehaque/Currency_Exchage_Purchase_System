8662337383f7b46eee25d4d2cdcf2890471b0c0d
Eya Jamue Haque <147173274+eyajamuehaque@users.noreply.github.com>
2024-05-12 01:22:18 +0600

    Update Congratulation.java

--- Congratulation.java
+++ Congratulation.java
@@ -3,15 +3,15 @@ import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Congratulation extends JFrame {
+public class Congratulation extends BaseFrame {
     JLabel label;
 
     public Congratulation(String username, String purchaseAmount, String companyName, String paymentMethod) {
         super("Congratulations!");
         this.setSize(600, 400);
         this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.getContentPane().setBackground(new Color(123, 104, 238)); 
+        this.setDefaultCloseOperation(BaseFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setBackground(new Color(123, 104, 138)); 
 
         
         JPanel panel = new JPanel(new GridBagLayout());
@@ -25,21 +25,18 @@ public class Congratulation extends JFrame {
         label = new JLabel("<html><div style='text-align: center;'><h1 style='color: #fff;'>Congratulations!</h1>"
                 + "<br><div style='font-size: 18px; color: #fff;'>"
                 + "Your purchase amount is " + purchaseAmount + "<br>"
-                + "Account holder: " + username + "<br>"
+               
                 + "Payment Company: " + companyName + "<br>"
                 + "Payment Method: " + paymentMethod + "<br>"
                 + "<strong>Time of Purchase:</strong> " + currentTime + "</div></div></html>");
         label.setForeground(Color.WHITE); 
 
-       
+     
         panel.add(label);
 
-        
+       
         this.add(panel, BorderLayout.CENTER);
     }
 
-    public static void main(String[] args) {
-        
-        java.awt.EventQueue.invokeLater(() -> new Congratulation("John Doe", "$100", "Company X", "Credit Card").setVisible(true));
-    }
+
 }
