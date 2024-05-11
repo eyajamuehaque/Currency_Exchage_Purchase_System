468c12041cd215064330609df2b226f0b46dac23
Eya Jamue Haque <147173274+eyajamuehaque@users.noreply.github.com>
2024-05-12 01:16:48 +0600

    Update form2.java

--- form2.java
+++ form2.java
@@ -1,39 +1,38 @@
+// form2.java
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class form2 extends JFrame implements ActionListener {
-    JPanel p;
-    JLabel l2, l3, l4, l;
-    JTextField un, pw, captchaInput;
-    JButton b1, b2, b3;
-    ImageIcon img;
+public class form2 extends BaseForm {
+    private JPanel p;
+    private JLabel l2, l3, l4, l;
+    private JButton b1, b2, b3;
+    private ImageIcon img;
 
     form2() {
         super("Sign In");
-        this.setSize(800, 600);
+		this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        
+       
         p = new JPanel(new BorderLayout()) {
             @Override
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
-                
+               
                 Image image = new ImageIcon("login.png").getImage();
                 g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
             }
         };
 
-        
+     
         JPanel loginPanel = new JPanel();
         loginPanel.setOpaque(false); 
         loginPanel.setLayout(null);
 
-        
-
+      
 
         l2 = new JLabel("User Name");
         l2.setFont(new Font("Verdana", Font.BOLD, 15));
@@ -90,22 +89,23 @@ public class form2 extends JFrame implements ActionListener {
         b2.addActionListener(this);
         loginPanel.add(b2);
 
-        
+      
         p.add(loginPanel, BorderLayout.CENTER);
 
         this.add(p);
     }
 
+   
     public void actionPerformed(ActionEvent ae) {
         if (ae.getSource() == b2) {
-            int userResult = Integer.parseInt(captchaInput.getText());
+            int userResult = Integer.parseInt(getCaptchaInput());
             int num1 = Integer.parseInt(l4.getText().split(": ")[1].split(" ")[0]);
             int num2 = Integer.parseInt(l4.getText().split(": ")[1].split(" ")[2]);
             int actualResult = Math.abs(num1 - num2);
 
             if (userResult == actualResult) {
-                String userName = un.getText();
-                String userPassword = pw.getText();
+                String userName = getUserName();
+                String userPassword = getUserPassword();
                 CreateAccount createAccount = new CreateAccount();
 
                 if (createAccount.getAccount(userName, userPassword)) {
@@ -125,5 +125,28 @@ public class form2 extends JFrame implements ActionListener {
             this.setVisible(false);
             f.setVisible(true);
         }
+    }
+
+    // Using polymorphic behavior for getting username
+    public String getUserName() {
+        return un.getText();
+    }
+
+    // Using polymorphic behavior for getting password
+    public String getUserPassword() {
+        return pw.getText();
+    }
+
+    // Using polymorphic behavior for getting captcha
+    public String getCaptchaInput() {
+        return captchaInput.getText();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            form2 f2 = new form2();
+            f2.setSize(800, 600);
+            f2.setVisible(true);
+        });
     }
 }
