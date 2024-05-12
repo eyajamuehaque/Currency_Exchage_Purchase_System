9258d825705978c1d9ef1f32d7c2d754838bcc24
Abdul Mubin Biswas <168845182+mubinbiswas2002@users.noreply.github.com>
2024-05-12 12:50:35 +0600

    BaseForm.java

--- BaseForm.java
+++ BaseForm.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-// BaseForm class is the base class for form2 and provides polymorphic behavior for username, password, and captcha
+
 
 public abstract class BaseForm extends JFrame implements ActionListener {
     protected JTextField un; 
@@ -13,26 +13,26 @@ public abstract class BaseForm extends JFrame implements ActionListener {
         super(title);
     }
 
-    // Abstract method to get the user input for username
+   
     public abstract String getUserName();
 
-    // Abstract method to get the user input for password
+    
     public abstract String getUserPassword();
 
-    // Abstract method to get the user input for captcha
+    
     public abstract String getCaptchaInput();
 
-    // Set method to set the username
+    
     public void setUserName(String userName) {
         un.setText(userName);
     }
 
-    // Set method to set the password
+    
     public void setUserPassword(String userPassword) {
         pw.setText(userPassword);
     }
 
-    // Set method to set the captcha input
+    
     public void setCaptchaInput(String captchaInput) {
         this.captchaInput.setText(captchaInput);
     }
