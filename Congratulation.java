a853be0b196fde3f848bfbce06d2430b5867eed4
Eya Jamue Haque <147173274+eyajamuehaque@users.noreply.github.com>
2024-05-14 19:25:55 +0600

    Update Congratulation.java

--- Congratulation.java
+++ Congratulation.java
@@ -11,30 +11,39 @@ public class Congratulation extends BaseFrame {
 
         this.getContentPane().setBackground(new Color(123, 104, 138)); 
 
-        
-        JPanel panel = new JPanel(new GridBagLayout());
+        JPanel panel = new JPanel(new BorderLayout());
         panel.setBackground(new Color(123, 104, 238));
 
-        
         SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
         String currentTime = sdf.format(new Date());
 
-       
         label = new JLabel("<html><div style='text-align: center;'><h1 style='color: #fff;'>Congratulations!</h1>"
                 + "<br><div style='font-size: 18px; color: #fff;'>"
                 + "Your purchase amount is " + purchaseAmount + "<br>"
-               
                 + "Payment Company: " + companyName + "<br>"
                 + "Payment Method: " + paymentMethod + "<br>"
                 + "<strong>Time of Purchase:</strong> " + currentTime + "</div></div></html>");
         label.setForeground(Color.WHITE); 
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setVerticalAlignment(SwingConstants.CENTER);
 
-     
-        panel.add(label);
+        panel.add(label, BorderLayout.CENTER);
 
-       
+        JButton feedbackButton = new JButton("Leave Feedback");
+        feedbackButton.addActionListener(e -> new Feedback(username));
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        buttonPanel.setBackground(new Color(123, 104, 238)); // Match background color
+        buttonPanel.add(feedbackButton);
+
+        panel.add(buttonPanel, BorderLayout.SOUTH);
+
         this.add(panel, BorderLayout.CENTER);
     }
 
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Congratulation("JohnDoe", "$150.00", "Visa", "Credit Card").setVisible(true);
+        });
+    }
 }
